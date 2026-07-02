a7a41b5b0501abaa6401195e6f583f17c27a39ec
Ajay-1017 <ajaybalu1026@gmail.com>
2026-07-02 12:13:17 +0530

    another method added

--- Logical_problems/1_remove_dup.py
+++ Logical_problems/1_remove_dup.py
@@ -13,12 +13,22 @@ def remove_dup(lst):
         seen.add(num)
     return ans
 
-
+# Method-2
+def remove_dup_1(lst):
+    i=0
+    j=1
+    while j<len(lst):
+        if lst[i]!=lst[j]:
+            lst[i+1] = lst[j]
+            i+=1
+        j+=1
+    return lst[:i]
 
 def main():
-    lst = [1,2,2,3,1]
+    lst = [1,2,2,3,1,1,1,1,1,1,1]
     print(remove_dup(lst))
+    print(remove_dup_1(lst))
 main()
 
 
-        
+            
